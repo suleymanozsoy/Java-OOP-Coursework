1b831e331752d5a1dae223184ad8a2f46c7ba824
Süleyman Özsoy <ozsoysuleyman119@gmail.com>
2026-01-27 17:01:32 +0300

    Dosya düzeni: ch02 klasörlemesi yapıldı ve Ödev-4 eklendi

---
+++ ch02/Odev04/Deneme.java
@@ -0,0 +1,13 @@
+public class Deneme {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 8;
+        double c = 4.5;
+        double d = 3.4;
+
+        System.out.println(a + b);
+        System.out.println(c + d);
+        System.out.println(a + c);
+    }
+}
